7a95acece1b0e4b837f6dac947db5e618f96172f
喵喵喵 <15235779164@163.com>
2019-05-24 14:18:11 +0800

    1

--- Take_Out_Project_MVC/Controllers/ZQController.cs
+++ Take_Out_Project_MVC/Controllers/ZQController.cs
@@ -43,7 +43,8 @@ namespace Take_Out_Project_MVC.Controllers
         [AuthorFilter]
         public ActionResult ZQYH()
         {
-            //string i = id.ToString();
+            ViewBag.s = 0;
+            ViewBag.ss = 0;
             HttpCookie co = Request.Cookies["UserId"];
             string id = co.Value;
             var json = HttpClientHelper.Sender("get", "ZQApi/ShowYH/" + id);
